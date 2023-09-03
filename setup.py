9bc453c03038bf65a447875b2517fbcf01e7378c
Kyle DeProw <kydepro@gmail.com>
2023-09-03 17:09:21 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this competition is to predict MDS-UPDR scores, which measure progression in patients with Parkinson's disease',
+    author='Kyle J DeProw',
+    license='MIT',
+)
